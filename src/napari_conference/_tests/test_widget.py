4f7e892580798806ad20466d35328d00f7098ad5
Kyle Harrington <czi@kyleharrington.com>
2026-06-07 08:37:48 -0700

    Fix test: MagicFactory does not accept positional args

--- src/napari_conference/_tests/test_widget.py
+++ src/napari_conference/_tests/test_widget.py
@@ -4,7 +4,6 @@ from napari_conference import conference_widget
 def test_conference_widget(make_napari_viewer, capsys):
     viewer = make_napari_viewer()
 
-    # this time, our widget will be a MagicFactory or FunctionGui instance
-    my_widget = conference_widget(viewer)
+    my_widget = conference_widget()
 
     assert my_widget is not None
